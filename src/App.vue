1f07747723880391396d529e0aae394a33393357
Yurii Rybachok <drummer2009@ukr.net>
2022-12-27 13:46:21 +0200

    page component layout

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,12 @@
 </script>
 
 <template>
-  <figure class="bg-blue-100 rounded-xl p-8 dark:bg-blue-800">
-    <img 
-      class="w-24 h-24 rounded-f"
-      src="https://tailwindcss.com/_next/static/media/sarah-dayan.a620c98f.jpg" 
-      alt="" 
-      width="384" 
-      height="512">
-    <div class="pt-6 space-y-4">
-      <blockquote>
-        <p class="text-lg">
-          “Tailwind CSS is the only framework that I've seen scale
-          on large teams. It’s easy to customize, adapts to any design,
-          and the build size is tiny.”
-        </p>
-      </blockquote>
-      <figcaption>
-        <div>Sarah Dayan</div>
-        <div>Staff Engineer, Algolia</div>
-      </figcaption>
+  <div class="flex h-screen flex-col">
+    <div class="flex flex-1 overflow-hidden">
+      <aside>Aside</aside>
+      <main class="min-w-0 lg:flex flex-1">
+        <router-view />
+      </main>
     </div>
-  </figure>
+  </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
