634e383737d7ffea6b03571bdc63cada5f0f9b14
Yurii Rybachok <drummer2009@ukr.net>
2022-12-28 18:11:01 +0200

    component tabs

---
+++ src/components/Tabs.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { Tab } from '../types';
+
+defineProps<{
+    tabs: Tab[];
+}>();
+</script>
+
+<template>
+    <div>
+        <nav class="flex space-x-4">
+            <a v-for="{id, emoji, title} in tabs" :key="id">
+                {{ emoji }}
+                {{ title }}
+            </a>
+        </nav>
+    </div>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,14 @@
 <script lang="ts" setup>
 import Page from '../components/layout/Page.vue';
+import Tabs from '../components/Tabs.vue'
+import { tabs } from '../mocks/tabs';
 </script>
 
 <template>
     <page>
         <template #title>Home</template>
+        <div>
+            <tabs :tabs="tabs" />
+        </div>
     </page>
 </template>
