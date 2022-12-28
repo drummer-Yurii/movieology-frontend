2cde6a0876b06891966e151967726c165a08c1b3
Yurii Rybachok <drummer2009@ukr.net>
2022-12-28 18:25:33 +0200

    classes for tabs

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -9,7 +9,11 @@ defineProps<{
 <template>
     <div>
         <nav class="flex space-x-4">
-            <a v-for="{id, emoji, title} in tabs" :key="id">
+            <a 
+                class="bg-movie-black-200 rounded-lg px-5 py-2 text-white drop-shadow-sm hover:bg-oranje cursor-pointer" 
+                v-for="{ id, emoji, title } in tabs" 
+                :key="id"
+            >
                 {{ emoji }}
                 {{ title }}
             </a>
