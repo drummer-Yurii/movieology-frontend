e990e293a880eab70ed8d78d8aae6bb19a555b97
Yurii Rybachok <drummer2009@ukr.net>
2022-12-28 18:48:44 +0200

    active push for tabs

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -2,16 +2,23 @@
 import { Tab } from '../types';
 
 defineProps<{
-    tabs: Tab[];
+    tabs: Tab[]
+    activeTab: string
 }>();
+
+const emits = defineEmits<{
+    (e: 'on-tab-change', tab: string): void
+}>()
 </script>
 
 <template>
     <div>
         <nav class="flex space-x-4">
             <a 
-                class="bg-movie-black-200 rounded-lg px-5 py-2 text-white drop-shadow-sm hover:bg-oranje cursor-pointer" 
+                class="text-lg rounded-lg px-5 py-2 text-white drop-shadow-sm hover:bg-oranje cursor-pointer" 
+                :class="id === activeTab ? 'bg-oranje' : 'bg-movie-black-200'"
                 v-for="{ id, emoji, title } in tabs" 
+                @click="emits('on-tab-change', id)"
                 :key="id"
             >
                 {{ emoji }}
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,24 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import Page from '../components/layout/Page.vue';
 import Tabs from '../components/Tabs.vue'
-import { tabs } from '../mocks/tabs';
+import { tabsMock } from '../mocks/tabs';
+
+const activeTab = ref<string>(tabsMock[0].id)
+const onTabChange = (tabId: string) => {
+    activeTab.value = tabId
+}
 </script>
 
 <template>
     <page>
         <template #title>Home</template>
         <div>
-            <tabs :tabs="tabs" />
+            <tabs 
+                :tabs="tabsMock" 
+                :active-tab="activeTab" 
+                @on-tab-change="onTabChange" 
+            />
         </div>
     </page>
 </template>
