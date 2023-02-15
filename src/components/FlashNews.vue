3274d3ec0a056f9d5d2245377e2d2028e679f8f7
Yurii Rybachok <drummer2009@ukr.net>
2023-02-15 11:37:31 +0200

    add slider to flesh news card

--- src/components/FlashNews.vue
+++ src/components/FlashNews.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
-import { ArrowRightIcon } from '@heroicons/vue/24/solid';
+import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/vue/24/solid';
+import { computed, ref } from 'vue';
 import { flashNews } from '../mocks/news';
 import Stack from './Stack.vue';
 
-const currentSlide = flashNews[0]
+interface FlashNews {
+    id: number;
+    rotate: number;
+    image: string;
+    text: string;
+    date: string;
+}
+
+const currentSlideIndex = ref<number>(0)
+const currentSlide = computed(() => flashNews[currentSlideIndex.value])
+const isLastSlide = computed(() => currentSlideIndex.value >= flashNews.length - 1)
+
+const nextSlide = () => {
+    if (isLastSlide.value) {
+        return;
+    }
+    currentSlideIndex.value++
+}
+
+const prevSlide = () => {
+    if (currentSlideIndex.value === 0) {
+        return;
+    }
+    currentSlideIndex.value--
+}
 </script>
 <template>
     <stack>
@@ -17,7 +42,12 @@ const currentSlide = flashNews[0]
                 <p class="text-gray-400 w-full">
                     {{ currentSlide.date }}
                 </p>
-                <p class="text-gray-900 flex">1/5 <ArrowRightIcon class="w-6 ml-2" /></p>
+                <p class="text-gray-900 flex">
+                    <ArrowLeftIcon v-if="currentSlideIndex > 0" class="w-6 mr-2" @click="prevSlide" />
+                    {{ currentSlideIndex + 1 }}/{{ flashNews.length }} 
+                    <ArrowRightIcon v-if="!isLastSlide" class="w-6 ml-2" @click="nextSlide" />
+                    <div v-else class="w-6 ml-2"></div>
+                </p>
             </div>
         </div>
     </stack>
