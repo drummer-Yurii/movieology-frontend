dc879c662441f3b7e93e86035571d6cb714aa88b
Yurii Rybachok <drummer2009@ukr.net>
2023-02-15 10:19:30 +0200

    add card with img to flash news component

--- src/components/FlashNews.vue
+++ src/components/FlashNews.vue
@@ -2,10 +2,15 @@
 import { flashNews } from '../mocks/news';
 import Stack from './Stack.vue';
 
+const currentSlide = flashNews[0]
 </script>
 <template>
     <stack>
-
+        <div class="w-full h-full bg-white rounded-2xl p-6 drop-shadow-md rotate-3">
+            <div>
+                <img :src="currentSlide.image" class="rounded-2xl aspect-video" alt="flash news poster">
+            </div>
+        </div>
     </stack>
 </template>
 
