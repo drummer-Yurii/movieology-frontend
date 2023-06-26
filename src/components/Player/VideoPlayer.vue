1ca5c6d7eb0ba25f95d6b750c7d23e047b577756
Yurii Rybachok <drummer2009@ukr.net>
2023-06-26 10:36:53 +0300

    create video player & add to page

---
+++ src/components/Player/VideoPlayer.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="player" ref="player">
+        <video 
+            class="video-screen" 
+            src="../../assets/video.mp4" 
+            controls
+            preload="auto"
+            ref="video"
+            allowfullscreen
+            type="video/mp4"
+        />
+        <time-control />
+        <volume-control />
+    </div>
+</template>
+
+<style>
+.player {
+    @apply w-full rounded-lg aspect-video relative object-cover cursor-pointer; 
+}
+
+.video-screen {
+    @apply aspect-video shadow-sm block w-full h-full cursor-pointer bg-movie-black-200;
+}
+
+::-webkit-media-controls {
+    display: none;
+}
+</style>
--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -7,20 +7,16 @@ defineProps<{
 }>();
 
 const emits = defineEmits<{
-    (e: 'on-tab-change', tab: string): void
+    'on-tab-change': [tab: string]
 }>()
 </script>
 
 <template>
     <div>
         <nav class="flex space-x-4">
-            <a 
-                class="text-lg rounded-lg px-5 py-2 text-white drop-shadow-sm hover:bg-oranje cursor-pointer" 
-                :class="id === activeTab ? 'bg-oranje' : 'bg-movie-black-200'"
-                v-for="{ id, emoji, title } in tabs" 
-                @click="emits('on-tab-change', id)"
-                :key="id"
-            >
+            <a class="text-lg rounded-lg px-5 py-2 text-white drop-shadow-sm hover:bg-oranje cursor-pointer"
+                :class="id === activeTab ? 'bg-oranje' : 'bg-movie-black-200'" v-for="{ id, emoji, title } in tabs"
+                @click="emits('on-tab-change', id)" :key="id">
                 {{ emoji }}
                 {{ title }}
             </a>
